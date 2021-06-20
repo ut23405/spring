8da3c307cc349fee75e7fe65a09da5936ba36469
ut23405 <ut23405@gmail.com>
2021-06-21 00:24:10 +0900

    2

---
+++ spring5-xml-hello-Component/src/com/example/test1/MainController.java
@@ -0,0 +1,17 @@
+package com.example.test1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainController {
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+
+//		ISyain syain = context.getBean("testSyain", ISyain.class);
+		ISyain syain = context.getBean("syain", ISyain.class);
+
+		System.out.println(syain.getHello()); //Hello World!
+		context.close();
+	}
+}
