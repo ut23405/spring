09ed4eb40556495b079f36c580f26767333782f1
ut23405 <ut23405@gmail.com>
2021-10-30 23:19:33 +0900

    spring5-mvc-hello

--- spring5-mvc-hello/src/main/java/com/example/test1/MainController.java
+++ spring5-mvc-hello/src/main/java/com/example/test1/MainController.java
@@ -1,6 +1,5 @@
 package com.example.test1;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class MainController {
-	
-	@Value("${item.upload.title}")
-	private String fileTest;
-	
+
 	@GetMapping("/abc")
 	public String hello(Model model) {
-		System.out.println(fileTest);
+		model.addAttribute("moji", "hello world!");
 		return "/test1/index";
 	}
 }
